9a92da56fd2951caf4e0a75cab6b9c015076ade5
Adam Akiva <adam.akiva@infinitylabs.co.il>
2024-01-24 18:05:19 +0200

    Minor script changes

--- templates/node/scripts/remove.sh
+++ templates/node/scripts/remove.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+####################################################################################
+
+DB_MIGRATIONS_FOLDER=../be/db-migrations
+DB_DATA_FOLDER=../db-dev-data
+TEST_COVERAGE_FOLDER=../be/__tests__/coverage
+FE_MODULES_FOLDER=../fe_node_modules
+BE_MODULES_FOLDER=../be/node_modules
+
+####################################################################################
+
 check_prerequisites() {
     if ! docker --version 1> /dev/null 2> /dev/null; then
         printf "docker engine not installed, you may follow this: https://docs.docker.com/engine/install\n\n" && exit 1;
@@ -12,13 +22,11 @@ check_prerequisites() {
 }
 
 remove() {
-    if ! INIT_SCRIPT=$(find $(pwd) ../be/$DB_MIGRATIONS_FOLDER -name "*.sql"); then
+    if ! INIT_SCRIPT=$(find $(pwd) $DB_MIGRATIONS_FOLDER -name "*.sql"); then
         printf "\nMigrations file not found. Did you follow the instructions correctly?\n\n" && exit 1;
     fi
 
-    if ! HOST_UID=$(id -u) \
-         HOST_GID=$(id -g) \
-         INIT_SCRIPT=$INIT_SCRIPT \
+    if ! HOST_UID=$(id -u) HOST_GID=$(id -g) INIT_SCRIPT=$INIT_SCRIPT \
          docker compose down; then
         printf "\ndocker removal failed, solve the error/s and try again\n\n" && exit 1;
     fi
@@ -30,7 +38,7 @@ remove_test_coverage() {
     printf "Do you wish to remove tests coverage folder? (y/n) ";
     read opn;
     if [ "${opn:-n}" = "y" ]; then
-        rm -rf ../be/__tests__/coverage;
+        rm -rf $TEST_COVERAGE_FOLDER;
     fi
 
     return 0;
@@ -40,7 +48,7 @@ remove_node_modules() {
     printf "Do you wish to remove node_modules folder? (y/n) ";
     read opn;
     if [ "${opn:-n}" = "y" ]; then
-        rm -rf ../be/node_modules ../be/.npm;
+        rm -rf $FE_MODULES_FOLDER $BE_MODULES_FOLDER
     fi
 
     return 0;
@@ -50,7 +58,7 @@ remove_database() {
     printf "Do you wish to remove database folder? (requires sudo permissions) (y/n) ";
     read opn;
     if [ "${opn:-n}" = "y" ]; then
-        sudo rm -rf ../db-dev-data;
+        sudo rm -rf $DB_DATA_FOLDER;
     fi
 
     return 0;
--- templates/node/scripts/start.sh
+++ templates/node/scripts/start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+####################################################################################
+
+DB_MIGRATIONS_FOLDER=../be/db-migrations
+DB_DATA_FOLDER=../db-dev-data
+
+####################################################################################
+
 check_prerequisites() {
     if ! docker --version 1> /dev/null 2> /dev/null; then
         printf "docker engine not installed, you may follow this: https://docs.docker.com/engine/install\n\n" && exit 1;
@@ -12,14 +19,12 @@ check_prerequisites() {
 }
 
 start() {
-    if ! INIT_SCRIPT=$(find $(pwd) ../be/$DB_MIGRATIONS_FOLDER -name "*.sql"); then
+    if ! INIT_SCRIPT=$(find $(pwd) $DB_MIGRATIONS_FOLDER -name "*.sql"); then
         printf "\nMigrations file not found. Did you follow the instructions correctly?\n\n" && exit 1;
     fi
 
-    printf "Building Application...\n\n" && mkdir -p ../db-dev-data;
-    if ! HOST_UID=$(id -u) \
-         HOST_GID=$(id -g) \
-         INIT_SCRIPT=$INIT_SCRIPT \
+    printf "Building Application...\n\n" && mkdir -p $DB_DATA_FOLDER;
+    if ! HOST_UID=$(id -u) HOST_GID=$(id -g) INIT_SCRIPT=$INIT_SCRIPT \
          docker -D compose up --build --remove-orphans \
          --always-recreate-deps --force-recreate -d --wait; then
         printf "\ndocker build failed, solve the error/s and try again\n\n" && exit 1;
